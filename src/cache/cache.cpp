2a11368b39b47e3086d2301c0183824ed4f341d2
Kadayam, Hari(hkadayam) <hkadayam@ebay.com>
2017-10-20 08:55:19 -0700

    Initial code for cache and rename namespace to omstore

---
+++ src/cache/cache.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Kadayam, Hari on 19/10/17.
+//
+
+#include "cache.h"
+
+namespace omstore {
+
+} // namespace omstore
---
+++ src/cache/cache.h
@@ -0,0 +1,20 @@
+//
+// Created by Kadayam, Hari on 19/10/17.
+//
+
+#ifndef OMSTORAGE_CACHE_HPP
+#define OMSTORAGE_CACHE_HPP
+namespace omstore {
+
+template <typename K, typename V>
+class Cache {
+
+};
+
+template <typename K, typename V>
+class IntrusiveCache : public Cache<K, V> {
+
+};
+
+} // namespace omstore
+#endif //OMSTORAGE_CACHE_HPP
