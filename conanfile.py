c00e9dcf9ce1ea2cefa16254b52515df22ad426f
Brian Szmyd <bszmyd@ebay.com>
2018-05-15 15:24:54 -0600

    v1.0.5 - add -Dlinux flag.

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "0.1.4"
+    version = "0.1.5"
     license = "Proprietary"
     description = "HomeStore"
 
@@ -13,11 +13,11 @@ class HomestoreConan(ConanFile):
     options = {"shared": [True, False]}
 
     requires = (("boost/[>=1.67,<2.0]@demo/dev"),
+                ("farmhash/[>=0.0,<1.0]@demo/dev"),
                 ("folly/[>=0.58,<1.0]@demo/dev"),
                 ("iomgr/[>=1.0,<2.0]@demo/dev"))
 
-    build_requires = (("farmhash/[>=0.0,<1.0]@demo/dev"),
-                      ("sds_logging/[>=0.1.2,<1.0]@demo/dev"))
+    build_requires = (("sds_logging/[>=0.1.2,<1.0]@demo/dev"))
 
     generators = "cmake"
     default_options = "shared=True"
@@ -30,6 +30,9 @@ class HomestoreConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="src", keep_path=True)
+        self.copy("*.hpp", dst="include", src="src", keep_path=True)
+        self.copy("*/btree_node.cpp", dst="include", src="src", keep_path=True)
+        self.copy("*cache/cache.cpp", dst="include", src="src", keep_path=True)
         self.copy("*.a", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
 
