79404ad1493bc763f1aede9fbd424fbe3783511c
Brian Szmyd <bszmyd@ebay.com>
2018-05-15 13:22:54 -0600

    v0.1.4 - Add pkg info

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,15 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "0.1.3"
+    version = "0.1.4"
     license = "Proprietary"
     description = "HomeStore"
 
-    settings = "os", "compiler", "build_type", "arch"
+    settings = {"os": ["Linux"],
+                "compiler": None,
+                "build_type": None,
+                "arch": None}
     options = {"shared": [True, False]}
 
     requires = (("boost/[>=1.67,<2.0]@demo/dev"),
@@ -29,3 +32,7 @@ class HomestoreConan(ConanFile):
         self.copy("*.h", dst="include", src="src", keep_path=True)
         self.copy("*.a", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs.extend(["aio", "double-conversion"])
