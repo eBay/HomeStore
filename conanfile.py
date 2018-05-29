977a681e251dec510e0cf72f5b293c0c0c411957
Brian Szmyd <bszmyd@ebay.com>
2018-05-29 13:35:58 -0600

    Use iomgr > 1.0.4

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class HomestoreConan(ConanFile):
     requires = (("boost/[>=1.67,<2.0]@oss/stable"),
                 ("farmhash/[>=0.0,<1.0]@oss/stable"),
                 ("folly/[>=0.58,<1.0]@oss/stable"),
-                ("iomgr/[>=1.0,<2.0]@demo/dev"),
+                ("iomgr/[>=1.0.4,<2.0]@demo/dev"),
                 ("benchmark/[>=1.4,<2.0]@oss/stable"))
 
     build_requires = (("sds_logging/[>=0.1.2,<1.0]@demo/dev"),
