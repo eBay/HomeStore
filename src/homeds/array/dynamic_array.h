038d852e7daced2cf1f6ec8f7d8a9df250b63227
Amit Desai <amdesai@ebay.com>
2018-07-13 17:01:59 -0700

    Save point

--- src/homeds/array/dynamic_array.h
+++ src/homeds/array/dynamic_array.h
@@ -2,113 +2,207 @@
  * Copyright eBay Inc 2018
  */
 
-#ifndef DYNAMIC_ARRAY_DS_H_
-#define DYNAMIC_ARRAY_DS_H_
+#ifndef SORTED_DYNAMIC_ARRAY_DS_H_
+#define SORTED_DYNAMIC_ARRAY_DS_H_
+
+#include <stdint.h>
+#include <cassert>
+#include <iostream>
+#include <cstring>
+#include <assert.h>
 
 
 namespace homeds {
 
-#include <cstring>
-#include "malloc.h"
-#include <assert.h>
-#include <string.h>
+
+#define SortedDynamicArrayType typename ElementType, int INITIAL_CAPACITY, int LOAD_PERCENT, int GROWTH_PERCENT
+#define SortedDynamicArrayTypeParams ElementType, INITIAL_CAPACITY, LOAD_PERCENT, GROWTH_PERCENT
 
     //TODO - MAKE CLASS THREAD SAFE
 
     /**
      * Array that grows in size as needed(Contigious memory). But never degrows.
+     * It always keeps all elements sorted. Element type needs to have compare operator defined.
      * @tparam T - class of element that array will hold
      */
-    template<typename T>
-    class Dynamic_Array {
+    template<SortedDynamicArrayType>
+    class Sorted_Dynamic_Array {
     private:
-        T *array;
-        int size;
-        int capacity;
-        float load_factor;
-        float growth_rate;
+        ElementType *m_array;
+        uint64_t m_size;
     public:
+        Sorted_Dynamic_Array();
 
-        Dynamic_Array(int initial_capacity, float load_factor, float growth_rate);
+        Sorted_Dynamic_Array(void *array, uint64_t size);
 
-        ~Dynamic_Array();
+        ~Sorted_Dynamic_Array();
 
-        void insert_shift(int pos, T element);
+        //add element into array
+        void add(ElementType element);
 
-        void remove_shift(int pos);
+        //removes element into array
+        void remove(ElementType element);
 
-        int get_size();
+        int get_size() const;
 
-        T& operator[](int pos);
+        ElementType &operator[](int pos);
+
+        ElementType *get_mem(void) const;
+
+        void set_mem(void *array, uint64_t size);
+
+        int get_no_of_elements() const;
 
         enum exception {
             MEMFAIL
         };
+
+        std::string get_string_representation();
+
     private:
+        //resizes array based on growth percent
         void resize_array();
 
+        //checks load perce t and determines if resize needed
         bool is_resize_required();
+
+        //right shifts all elements from pos to pos+1. And sets element at pos.
+        void insert_shift(int pos, ElementType element);
+
+        //removes element at pos and left shifts all succeding elements
+        void remove_shift(int pos);
+
+        // if found - returns +ve index of element
+        // if not found - returns -ve index where element should be inserted
+        int binary_search(ElementType element);
+
+        //update element in place
+        void update(int pos, ElementType element);
+
     };
 
-    template<typename T>
-    Dynamic_Array<T>::Dynamic_Array(int initial_capacity, float load_factor, float growth_rate):capacity(
-            initial_capacity), load_factor(load_factor), growth_rate(growth_rate) {
-        if (load_factor < 0 || load_factor > 1 || growth_rate > 1 || growth_rate < 0) assert(0);
-        array = (T *) malloc(initial_capacity * sizeof(T));
-        if (array == NULL)
-            throw MEMFAIL;
-        size = initial_capacity;
+    template<SortedDynamicArrayType>
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::Sorted_Dynamic_Array() {
+        this->m_size = INITIAL_CAPACITY * sizeof(ElementType);
+        this->m_array = (ElementType *) malloc(m_size);
     }
 
-    template<typename T>
-    Dynamic_Array<T>::~Dynamic_Array() {
-        if (array) {
-            free(array);
-            array = NULL;
+    template<SortedDynamicArrayType>
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::Sorted_Dynamic_Array(void *array, uint64_t size) {
+        m_array = malloc(size);
+        memcpy(m_array, array, size);
+        m_size = size;
+    }
+
+    template<SortedDynamicArrayType>
+    void Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::set_mem(void *array, uint64_t size) {
+        this->m_array = array;
+        this->m_size = size;
+    }
+
+    template<SortedDynamicArrayType>
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::~Sorted_Dynamic_Array() {
+        if (m_array) {
+            free(m_array);
+            m_array = NULL;
         }
     }
 
-    template<typename T>
-    int Dynamic_Array<T>::get_size() {
-        return size;
+    template<SortedDynamicArrayType>
+    int
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::get_size() const {
+        return m_size;
     }
 
-    template<typename T>
-    void Dynamic_Array<T>::remove_shift(int pos) {
-        if (pos >= size || pos < 0) assert(0);
-        memmove(array + pos + 1, array + pos, size - pos - 1);
-        size--;
+    template<SortedDynamicArrayType>
+    int
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::get_no_of_elements() const {
+        return m_size / sizeof(ElementType);
     }
 
-    template<typename T>
-    void Dynamic_Array<T>::insert_shift(int pos, T element) {
-        if (pos >= size || pos < 0) assert(0);
-        if (is_resize_required()) {
-            resize_array();
-        }
-        memmove(array + pos + 1, array + pos, size - pos);
-        size++;
+    template<SortedDynamicArrayType>
+    void
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::add(ElementType element) {
+        int pos = binary_search(element);
+        if (pos < 0)
+            insert_shift(pos, element);
+        else
+            update(pos, element);
     }
 
-    template<class T>
-    T &Dynamic_Array<T>::operator[](int pos) {
-        if (pos >= size || pos < 0) assert(0);
-        return *(array + pos);
+    template<SortedDynamicArrayType>
+    void
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::remove(ElementType element) {
+
     }
 
-    template<class T>
-    bool Dynamic_Array<T>::is_resize_required() {
-        if (size / capacity >= load_factor) return true;
+    template<typename ElementType, int INITIAL_CAPACITY, int LOAD_PERCENT, int GROWTH_PERCENT>
+    ElementType
+    &Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::operator[](int pos) {
+        int m_no_of_elements = m_size / sizeof(ElementType);
+        if (pos >= m_no_of_elements || pos < 0) assert(0);
+        return *(m_array + pos);
+    }
+
+    template<typename ElementType, int INITIAL_CAPACITY, int LOAD_PERCENT, int GROWTH_PERCENT>
+    bool
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::is_resize_required() {
+        int m_no_of_elements = m_size / sizeof(ElementType);
+        if (m_no_of_elements / INITIAL_CAPACITY >= LOAD_PERCENT) return true;
         else return false;
     }
 
-    template<class T>
-    void Dynamic_Array<T>::resize_array() {
-        int new_capacity = capacity + capacity*growth_rate;
-        T *new_array = (T *) malloc(new_capacity * sizeof(T));
-        memcpy(new_array, array, size);
-        free(array);
-        array= new_array;
+    template<SortedDynamicArrayType>
+    void
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::resize_array() {
+        int m_no_of_elements = m_size / sizeof(ElementType);
+        int new_capacity = INITIAL_CAPACITY + INITIAL_CAPACITY * GROWTH_PERCENT;
+        ElementType *new_array = (ElementType *) malloc(new_capacity * sizeof(ElementType));
+        memcpy(new_array, m_array, m_no_of_elements);
+        free(m_array);
+        m_array = new_array;
+    }
+
+    template<SortedDynamicArrayType>
+    int
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::binary_search() {
+        return 0;
+    }
+
+    template<SortedDynamicArrayType>
+    void
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::remove_shift(int pos) {
+        int m_no_of_elements = m_size / sizeof(ElementType);
+        if (pos >= m_no_of_elements || pos < 0) assert(0);
+        memmove(m_array + pos + 1, m_array + pos, m_no_of_elements - pos - 1);
+    }
+
+    template<SortedDynamicArrayType>
+    void
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::insert_shift(int pos, ElementType element) {
+        int m_no_of_elements = m_size / sizeof(ElementType);
+        if (pos >= m_no_of_elements || pos < 0) assert(0);
+        if (is_resize_required()) {
+            resize_array();
+        }
+        memmove(m_array + pos + 1, m_array + pos, m_no_of_elements - pos);
+    }
+
+    template<SortedDynamicArrayType>
+    void
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::update(int pos, ElementType element) {
+
+    }
+
+    template<SortedDynamicArrayType>
+    ElementType *Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::get_mem(void) const {
+        return (void *) m_array;
+    }
+
+    template<SortedDynamicArrayType>
+    std::string
+    Sorted_Dynamic_Array<SortedDynamicArrayTypeParams>::get_string_representation() {
+        return std::__cxx11::string();
     }
 }
 
---
+++ src/mapping/mapping_var_value.h
@@ -0,0 +1,234 @@
+#include "homeds/btree/ssd_btree.hpp"
+#include "homeds/btree/btree.hpp"
+#include <blkalloc/blk.h>
+#include <csignal>
+#include <error/error.h>
+#include <homeds/array/dynamic_array.h>
+
+using namespace std;
+using namespace homestore;
+using namespace homeds::btree;
+
+#define Sorted_Dynamic_Array_Impl Sorted_Dynamic_Array<BlkId,3, 80, 20>
+        
+class MappingKey : public homeds::btree::BtreeKey {
+private:
+    uint64_t blob;
+    uint64_t *ptr_blob;
+public:
+    MappingKey(){}
+    MappingKey(uint64_t _blob) {
+        blob = _blob;
+        ptr_blob = &blob;
+    }
+    int compare(const BtreeKey *o) const override {
+        MappingKey *key = (MappingKey *)o;
+        if (*ptr_blob < *key->ptr_blob) {
+            return -1;
+        } else if (*ptr_blob > *key->ptr_blob) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    virtual homeds::blob get_blob() const override {
+        homeds::blob b = {(uint8_t *) ptr_blob, sizeof(blob)};
+        return b;
+    }
+    virtual void set_blob(const homeds::blob &b) override {
+        ptr_blob = (uint64_t *)b.bytes;
+    }
+    virtual void copy_blob(const homeds::blob &b) override {
+        memcpy(ptr_blob, b.bytes, b.size);
+    }
+    virtual uint32_t get_blob_size() const override {
+        return (sizeof(*ptr_blob));
+    }
+    virtual void set_blob_size(uint32_t size) override {
+    }
+    static uint32_t get_fixed_size() {
+        return sizeof(blob);
+    }
+    uint64_t get_value() {
+        return blob;
+    }
+    int compare_range(const BtreeSearchRange &range) const override {
+        return 0;
+    }
+    std::string to_string() const  {
+        return (std::to_string(*ptr_blob));
+    }
+};
+
+class MappingValue : public homeds::btree::BtreeValue {
+    // grow 20% on 80% load with inital size as 3
+    Sorted_Dynamic_Array_Impl dyna_arr;
+public:
+    MappingValue():dyna_arr() {
+    };
+    virtual homeds::blob get_blob() const override {
+        homeds::blob b;
+        b.bytes = (uint8_t *)dyna_arr.get_mem(); b.size = dyna_arr.get_size();
+        return b;
+    }
+    virtual void set_blob(const homeds::blob &b) override {
+        dyna_arr.set_mem((void *)(b.bytes), b.size);
+    }
+    virtual void copy_blob(const homeds::blob &b) override {
+        dyna_arr = Sorted_Dynamic_Array_Impl( b.bytes,b.size);
+    }
+    virtual void append_blob(const BtreeValue &new_val) override {
+       assert( ( (const Sorted_Dynamic_Array_Impl &)new_val).get_no_of_elements()==1);
+       dyna_arr.add ( ( (const Sorted_Dynamic_Array_Impl &)new_val)[0] );
+    }
+    virtual uint32_t get_blob_size() const override {
+        return dyna_arr.get_size();
+    }
+    virtual void set_blob_size(uint32_t size) override {
+        assert(0);
+    }
+    struct BlkId get_val(uint64_t lba, uint32_t nblks,
+                         std::vector<BlkId> &blkIdList) {
+        return nullptr;
+    }
+    std::string to_string() const  {
+        return dyna_arr.get_string_representation();
+    }
+};
+
+#define MappingBtreeDeclType     homeds::btree::Btree<homeds::btree::SSD_BTREE, MappingKey, MappingValue, \
+                                    homeds::btree::BTREE_NODETYPE_VAR_VALUE, homeds::btree::BTREE_NODETYPE_VAR_VALUE, 4096u>
+#define KEY_RANGE	1
+constexpr auto MAP_BLOCK_SIZE = (4 * 1024ul);
+
+namespace homestore {
+    struct lba_BlkId_mapping {
+        uint64_t lba;
+        BlkId blkId;
+        bool blkid_found;
+        lba_BlkId_mapping():lba(0),blkId(0),blkid_found(false){};
+    };
+}
+
+class mapping {
+    typedef std::function< void (struct BlkId blkid) > free_blk_callback;
+private:
+    MappingBtreeDeclType *m_bt;
+
+    constexpr static auto Ki = 1024;
+    constexpr static auto Mi = Ki * Ki;
+    constexpr static auto Gi = Ki * Mi;
+    constexpr static auto MAX_CACHE_SIZE = 2ul * Gi;
+
+    free_blk_callback free_blk_cb;
+public:
+    mapping(uint32_t volsize, free_blk_callback cb, DeviceManager *mgr) :  free_blk_cb(cb) {
+
+        homeds::btree::BtreeConfig btree_cfg;
+        btree_cfg.set_max_objs(volsize/(KEY_RANGE*MAP_BLOCK_SIZE));
+        btree_cfg.set_max_key_size(sizeof(MappingKey));
+        btree_cfg.set_max_value_size(sizeof(MappingValue));
+
+        //TODO: we want to initialize btree_device_info only in case of SSD tree
+
+        // Create a global cache entry
+        homestore::Cache< BlkId > *glob_cache = new homestore::Cache< homestore::BlkId >(MAX_CACHE_SIZE, MAP_BLOCK_SIZE);
+        assert(glob_cache);
+
+
+        homeds::btree::btree_device_info bt_dev_info;
+        bt_dev_info.new_device = true;
+        bt_dev_info.dev_mgr = mgr;
+        bt_dev_info.size= 512 * Mi;
+        bt_dev_info.cache = glob_cache;
+        bt_dev_info.vb = nullptr;
+        m_bt = MappingBtreeDeclType::create_btree(btree_cfg, &bt_dev_info);
+    }
+
+    MappingKey get_key(uint32_t lba) {
+        MappingKey key(lba/KEY_RANGE);
+        return key;
+    }
+
+    MappingValue get_value(struct BlkId blkid) {
+        MappingValue value(blkid);
+        return value;
+    }
+
+    std::error_condition put(uint64_t lba, uint32_t nblks, struct BlkId blkid) {
+        MappingValue value;
+
+        /* TODO: It is very naive way of doing it and will definitely impact
+         * the performance. We have a new design and will implement it with
+         * snapshot.
+         */
+        for (uint32_t i = 0; i < nblks; ++i) {
+            blkid.set_nblks(1);
+            /* TODO: don't need to call remove explicitly once
+             * varsize btree is plugged in.
+             */
+            bool ret = m_bt->remove(get_key(lba), &value);
+            if (ret) {
+                /* free this block */
+                free_blk_cb(value.get_val());
+            }
+            m_bt->put(get_key(lba), get_value(blkid),
+                      homeds::btree::INSERT_ONLY_IF_NOT_EXISTS);
+            ++lba;
+            blkid.set_id(blkid.get_id() + 1);
+        }
+        return homestore::no_error;
+    }
+
+    std::error_condition get(uint64_t lba, uint32_t nblks,
+                             std::vector<struct homestore::lba_BlkId_mapping> &mappingList) {
+        std::error_condition error = no_error;
+        bool atleast_one_lba_found = false;
+        bool atleast_one_lba_not_found = false;
+        uint64_t key;
+
+        while (nblks != 0) {
+            homestore::lba_BlkId_mapping* mapping = new struct homestore::lba_BlkId_mapping();
+            mapping->lba = lba;
+            MappingValue value;
+
+            key = get_key(lba).get_value();
+            bool ret = m_bt->get(get_key(lba), &value);
+            if (!ret) {
+                mappingList.push_back(*mapping);
+                lba++;
+                nblks--;
+                atleast_one_lba_not_found = true;
+                continue;
+            }
+            atleast_one_lba_found = true;
+            mapping->blkId = value.get_val();
+            mapping->blkid_found = true;
+
+            uint32_t maxBlkRead = KEY_RANGE - (lba - (key * KEY_RANGE));
+
+            if (maxBlkRead >= nblks) {
+                mapping->blkId.set_nblks(nblks);
+                mapping->blkId.set_id(mapping->blkId.get_id() + lba - (key * KEY_RANGE));
+                mappingList.push_back(*mapping);
+                nblks = 0;
+            } else {
+                mapping->blkId.set_nblks(maxBlkRead);
+                mapping->blkId.set_id(mapping->blkId.get_id() + lba - (key * KEY_RANGE));
+                mappingList.push_back(*mapping);
+                nblks = nblks - maxBlkRead;
+                lba = lba + maxBlkRead;
+            }
+        }
+
+        if(!atleast_one_lba_found){
+            mappingList.empty();
+            error = homestore::make_error_condition(
+                    homestore_error::lba_not_exist);
+        }else if(atleast_one_lba_not_found){
+            error = homestore::make_error_condition(
+                    homestore_error::partial_lba_not_exist);
+        }
+        return error;
+    }
+};
