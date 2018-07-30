78f6895a6f92cc56b034385970f66b461eed4ae7
Brian Szmyd <bszmyd@ebay.com>
2018-07-30 17:18:55 +0000

    Bump folly to v2018.07.30.00

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class HomestoreConan(ConanFile):
                 ("boost_uuid/1.66.0@bincrafters/stable"),
                 ("double-conversion/3.0.0@bincrafters/stable"),
                 ("farmhash/1.0.0@oss/stable"),
-                ("folly/2018.07.23.00@bincrafters/stable"),
+                ("folly/2018.07.30.00@bincrafters/stable"),
                 ("gtest/1.8.0@bincrafters/stable"),
                 ("iomgr/1.1.2@sds/testing"))
 
