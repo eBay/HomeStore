84e8bb693b5093931ee005a6bde03e2a6ffdf0ee
Brian Szmyd <bszmyd@ebay.com>
2018-09-06 20:27:02 +0000

    Working dynamic module logging.

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class HomestoreConan(ConanFile):
                 ("folly/2018.08.20.00@bincrafters/stable"),
                 ("iomgr/2.0.4@sds/stable"),
                 ("sds_metrics/0.2.0@sds/testing"),
-                ("sds_logging/3.2.2@sds/stable"))
+                ("sds_logging/3.2.4@sds/stable"))
 
     generators = "cmake"
     exports_sources = "cmake/*", "src/*", "CMakeLists.txt"
