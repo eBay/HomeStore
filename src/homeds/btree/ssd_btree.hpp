7dd0317a4af5efd411188fe2eaf53674231e75ca
Hari Kadayam <hkadayam@ebay.com>
2019-02-19 18:40:51 -0800

    Fixed compilation issue after the merge

--- src/homeds/btree/ssd_btree.hpp
+++ src/homeds/btree/ssd_btree.hpp
@@ -234,7 +234,7 @@ public:
                            std::deque< boost::intrusive_ptr< homestore::writeback_req > >& dependent_req_q,
                            boost::intrusive_ptr< homestore::writeback_req > cookie, bool is_sync,
                            boost::intrusive_ptr< btree_multinode_req > multinode_req = nullptr) {
-        homestore::BlkId = blkid(bn->get_node_id().m_id);
+        homestore::BlkId blkid(bn->get_node_id().m_id);
         auto req = ssd_btree_req::make_object();
         req->is_read = false;
         req->cookie = cookie;
