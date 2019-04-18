8d7a0e6c118885ad6f665ef307a289f82e77a736
Yaming Kuang <yakuang@ebay.com>
2019-04-17 23:49:48 -0700

    enable sanitizer for map_testing and perf_vol_testing

--- src/volume/vol_gtest.cpp
+++ src/volume/vol_gtest.cpp
@@ -834,14 +834,6 @@ SDS_OPTION_GROUP(test_volume,
 #define ENABLED_OPTIONS logging, home_blks, test_volume
 SDS_OPTIONS_ENABLE(ENABLED_OPTIONS)
 
-/* it will go away once shutdown is implemented correctly */
-#if 0
-extern "C" 
-__attribute__((no_sanitize_address))
-const char* __asan_default_options() { 
-    return "detect_leaks=0"; 
-}
-#endif
 /************************** MAIN ********************************/
 
 /* We can run this target either by using default options which run the normal io tests or by setting different options.
