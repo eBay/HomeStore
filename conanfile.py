e12b32b6c9758b992397cea0ee602148740e057b
Hari Kadayam <harihara.kadayam@gmail.com>
2019-06-12 08:47:38 -0700

    Added missing conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
 
-    version = "0.11.11"
+    version = "0.11.12"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/SDS/Homestore"
