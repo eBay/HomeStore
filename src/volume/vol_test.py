95af96f95bd9e0d8222e37f289245b7657bca3aa
soungupta <soungupta@ebay.com>
2019-06-19 17:12:34 -0700

    fixed subprocess errors and individual logging issues in the regression chain

--- src/volume/vol_test.py
+++ src/volume/vol_test.py
@@ -14,16 +14,18 @@ for opt,arg in opts:
         print(("testing suits (%s)")%(arg))
 
 def normal():
-    output = subprocess.check_output("./test_volume --run_time=3600", shell=True)
-    if output == false:
-        print("Normal volume test error")
-    return output
+    status = subprocess.check_output("./test_volume --run_time=3600", shell=True)
+    f = open( '/home/homestore/log_normal.txt', 'w' )
+    f.write(status)
+    f.close()
+    return '[  PASSED  ] 1 test' in status
 
 def vol_delete():
-    output = subprocess.check_output("./test_volume --gtest_filter=*vol_del*", shell=True)
-    if output == false:
-        print("Delete volume test error")
-    return output
+    status = subprocess.check_output("./test_volume --gtest_filter=*vol_del*", shell=True)
+    f = open( '/home/homestore/log_delete.txt', 'w' )
+    f.write(status)
+    f.close()
+    return '[  PASSED  ] 1 test' in status
 
 def recovery():
     subprocess.call("./test_volume --gtest_filter=*normal_abort_random* --run_time=300 --install_crash=0", shell=True)
@@ -31,17 +33,16 @@ def recovery():
         subprocess.call("./test_volume --gtest_filter=*recovery_abort* --run_time=300 --install_crash=0", shell=True)
 
 def mapping():
-    output = subprocess.check_output("./test_mapping --num_ios=1000000", shell=True)
-    if output == false:
-        print("Mapping test error")
-    return output
+    status = subprocess.check_output("./test_mapping --num_ios=1000000", shell=True)
+    f = open( '/home/homestore/log_mapping.txt', 'w' )
+    f.write(status)
+    f.close()
+    return '[  PASSED  ] 1 test' in status
 
 def sequence():
-    if normal() == false:
+    if normal() == False:
         sys.exit(0)
-    if vol_delete() == false:
-        sys.exit(0)
-    if mapping() == false:
+    if mapping() == False:
         sys.exit(0)
 
 if test_suits == "normal":
