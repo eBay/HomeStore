4d4e42f407d4c2b4f837c5a051c2fdc16dde208e
soungupta <soungupta@ebay.com>
2019-07-10 14:11:11 -0700

    reverting to old code for jenkins build test

--- src/blkalloc/varsize_blk_allocator.cpp
+++ src/blkalloc/varsize_blk_allocator.cpp
@@ -304,7 +304,7 @@ BlkAllocStatus VarsizeBlkAllocator::alloc(uint8_t nblks,
         BLKALLOC_LOG(ERROR, , "blks_alloced != nblks : {}  {}",blks_alloced, nblks);
         BLKALLOC_LOG_ASSERT(blks_alloced < nblks);
         /* free blks */
-        for (auto it = out_blkid.begin(); it != out_blkid.end();) {
+        for (auto it = out_blkid.begin(); it != out_blkid.end(); ++it) {
             free(*it);
             it = out_blkid.erase(it);
         }
