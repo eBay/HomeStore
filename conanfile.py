77ffe27a9dbf921af9562ade02e7a20f459f9634
Brian Szmyd <bszmyd@ebay.com>
2019-08-22 11:53:40 -0700

    Start 0.11.18

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
 
-    version = "0.11.17"
+    version = "0.11.18"
 
 
     license = "Proprietary"
