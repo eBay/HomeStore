77c4fb1dab4e9ceab08cf851dcd8d6baf8d5881d
Brian Szmyd <bszmyd@ebay.com>
2019-09-06 08:51:55 -0700

    Start v0.11.19

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
 
-    version = "0.11.18"
-
+    version = "0.11.19"
+    revision_mode = "scm"
 
     license = "Proprietary"
     url = "https://github.corp.ebay.com/SDS/Homestore"
