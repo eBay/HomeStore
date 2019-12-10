2b684a5db323a96c90415153a21a23530e5626a9
rishabh mittal <rimittal@ebay.com>
2019-12-09 17:21:27 -0800

    remove extra changes

--- src/volume/volume.cpp
+++ src/volume/volume.cpp
@@ -114,8 +114,8 @@ void Volume::set_error_flip() {
     
     /* error flips */
     freq.set_percent(1);
-    fc->inject_retval_flip("delay_us_and_inject_error_on_completion", { null_cond }, freq, 20);
-//    fc->inject_noreturn_flip("varsize_blkalloc_no_blks", { null_cond }, freq);
+//    fc->inject_retval_flip("delay_us_and_inject_error_on_completion", { null_cond }, freq, 20);
+    fc->inject_noreturn_flip("varsize_blkalloc_no_blks", { null_cond }, freq);
     
 }
 
