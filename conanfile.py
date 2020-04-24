a2a01d842a86200864f6e92553d2e03d4f1d299a
Brian Szmyd <bszmyd@ebay.com>
2020-04-24 07:38:40 -0700

    Update sisl for bug fix.

--- conanfile.py
+++ conanfile.py
@@ -30,7 +30,7 @@ class HomestoreConan(ConanFile):
             "flip/0.2.7@sds/develop",
             "iomgr/2.2.14@sds/develop",
             "sds_logging/7.0.0@sds/develop",
-            "sisl/1.0.4@sisl/develop",
+            "sisl/1.0.6@sisl/develop",
 
             # FOSS, rarely updated
             "benchmark/1.5.0",
