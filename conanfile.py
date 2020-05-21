82ae3f5a0ee40a06b9027a7bc50e0c451e1dc66e
Brian Szmyd <bszmyd@ebay.com>
2020-05-21 02:46:49 +0000

    Start v0.13.1

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
 
-    version = "0.13.0"
+    version = "0.13.1"
     revision_mode = "scm"
 
     license = "Proprietary"
