c9d773fb2c7af2a08a5a42692646c02ea166baaf
rishabh mittal <rimittal@ebay.com>
2020-06-23 21:48:23 -0700

    fix build issue

---
+++ src/engine/homestore_base.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include <fds/malloc_helper.hpp>
+#include <boost/intrusive_ptr.hpp>
+#include "common/homestore_header.hpp"
+#include <metrics/metrics.hpp>
+
+const uint16_t init_crc_16 = 0x8005;
+typedef uint32_t crc32_t;
+static constexpr crc32_t init_crc32 = 0x12345678;
+static constexpr crc32_t INVALID_CRC32_VALUE = 0x0u;
+
+namespace homestore {
+
+struct blkalloc_cp_id;
+struct indx_cp_id;
+typedef std::shared_ptr< homestore::indx_cp_id > indx_cp_id_ptr;
+struct hs_cp_id;
+struct DeviceManager;
+class BlkBuffer;
+template < typename BAllocator, typename Buffer >
+class BlkStore;
+class VdevVarSizeBlkAllocatorPolicy;
+typedef homestore::BlkStore< homestore::VdevVarSizeBlkAllocatorPolicy, BlkBuffer > data_blkstore_t;
+typedef homestore::BlkStore< homestore::VdevVarSizeBlkAllocatorPolicy, BlkBuffer > logdev_blkstore_t;
+
+/* This class is introduced only to avoid template in any of its subsystem. Subsystem can get any homestore info other
+ * then indx blkstore from this base class.
+ */
+class HomeStoreBase {
+public:
+    using HomeStoreBaseSafePtr = boost::intrusive_ptr< HomeStoreBase >;
+
+private:
+    sisl::atomic_counter< uint64_t > m_usage_counter = 1;
+    static HomeStoreBaseSafePtr _instance;
+
+public:
+    virtual ~HomeStoreBase() = default;
+    friend void intrusive_ptr_add_ref(HomeStoreBase* hs) { hs->m_usage_counter.increment(1); }
+    friend void intrusive_ptr_release(HomeStoreBase* hs) {
+        // If there is only one reference remaining after decrementing, then we are done with shutdown, cleanup the
+        // _instance and delete the homeblks.
+        if (hs->m_usage_counter.decrement_test_eq(1)) {
+            auto p = HomeStoreBase::_instance.detach();
+            assert(p == hs);
+            delete hs;
+        }
+    }
+
+    static void set_instance(HomeStoreBaseSafePtr instance) { _instance = instance; }
+    static HomeStoreBase* instance() { return _instance.get(); }
+    static HomeStoreBaseSafePtr safe_instance() { return _instance; }
+    virtual data_blkstore_t* get_data_blkstore() const = 0;
+    virtual void attach_prepare_indx_cp_id(std::map< boost::uuids::uuid, indx_cp_id_ptr >* cur_id_map,
+                                           std::map< boost::uuids::uuid, indx_cp_id_ptr >* new_id_map, hs_cp_id* hs_id,
+                                           hs_cp_id* new_hs_id) = 0;
+    virtual void blkalloc_cp_start(std::shared_ptr< blkalloc_cp_id > id) = 0;
+    virtual std::shared_ptr< blkalloc_cp_id >
+    blkalloc_attach_prepare_cp(std::shared_ptr< blkalloc_cp_id > cur_cp_id) = 0;
+    virtual uint32_t get_data_pagesz() const = 0;
+    virtual DeviceManager* get_device_manager() = 0;
+    virtual logdev_blkstore_t* get_logdev_blkstore() const = 0;
+};
+} // namespace homestore
