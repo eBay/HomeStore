024fc214d377adf89a51e630449195866f3487bb
Yaming Kuang <yakuang@ebay.com>
2020-09-01 16:51:00 -0700

    replace abort to avoid core file generation (#559)
    * replace abort to avoid core file generation
    
    * update homestore version

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
 
-    version = "0.13.6"
+    version = "0.13.7"
     revision_mode = "scm"
 
     license = "Proprietary"
