3850dd8162837e05b394237791ea9cd1fcdaa594
Bryan Zimmerman <brzimmerman@ebay.com>
2020-10-02 16:25:06 -0400

    SDSTOR-3023: Bump version for interface change

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
 
-    version = "1.0.3"
+    version = "1.1.3"
     revision_mode = "scm"
 
     license = "Proprietary"
