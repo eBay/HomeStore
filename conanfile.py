540e40cf83821cb9a883e953003962cafb339e87
Yaming Kuang <yakuang@ebay.com>
2020-11-24 10:46:27 -0800

    SDSTOR-xxxx make homestore 2.x compatible with AM (#639)
    * SDSTOR-xxxx make homestore 2.x compatible with AM

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "2.1.4"
+    version = "2.1.5"
     revision_mode = "scm"
 
     license = "Proprietary"
