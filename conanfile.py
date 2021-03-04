eb3d9b5f34e49cdf5e49fb0b1ef261585559ff77
yakuang <yakuang@ebay.com>
2021-03-04 12:41:27 -0800

    SDSTOR-3972 Meta assert fail after random crash (#727)
    * SDSTOR-3972 fix assert fail after random crash in e2e test
    * remove debug code
    * fix a bug about offset

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class HomestoreConan(ConanFile):
     name = "homestore"
     
-    version = "2.4.13"
+    version = "2.4.14"
 
     revision_mode = "scm"
     license = "Proprietary"
