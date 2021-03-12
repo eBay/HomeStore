dd0c5cd70a46581af9dc9b6bf3d58b2dbfad6931
rishabh mittal <rimittal@ebay.com>
2021-03-12 10:07:06 -0800

    conan bump

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "2.4.18"
+    version = "2.4.19"
 
     revision_mode = "scm"
     license = "Proprietary"
