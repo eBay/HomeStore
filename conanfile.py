b281fc5997b8ed4f4a36bb1c6a0c078488a7b2d7
brzimmerman <brzimmerman@ebay.com>
2021-07-20 13:34:10 -0800

    SDSTOR-5150: Remove infinite loop if error encountered.  Move test conditions into while loop (#882)

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "2.7.13"
+    version = "2.7.14"
 
     revision_mode = "scm"
     license = "Proprietary"
