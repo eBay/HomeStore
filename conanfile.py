99b973c399ca987455bf2232efa3631d0be88989
yakuang <yakuang@ebay.com>
2021-11-29 16:00:24 -0800

    SDSTOR-xxxx move io watchdog creation regardless of safe-mode or non-safe-mode (#959)
    * set cp watch dog timer to 30 seconds for spdk nightly
    
    * fix iomgr log module
    
    * check for null instance
    
    * check for null instance
    
    * update con ver
    
    * fix
    
    * fix

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "3.0.12"
+    version = "3.0.13"
 
     revision_mode = "scm"
     license = "Proprietary"
