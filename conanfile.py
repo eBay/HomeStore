8b7f7dd7757689c5f06b629929bc796bc70afa94
rishabh mittal <rimittal@ebay.com>
2021-12-02 13:53:34 -0800

    modified conan version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "3.0.14"
+    version = "3.1.0"
 
     revision_mode = "scm"
     license = "Proprietary"
