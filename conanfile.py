3d228bd83bdb76e6288c6e9e18fd6cf56a1607ca
shosseinimotlagh <shosseinimotlagh@ebay.com>
2022-08-16 13:27:05 -0700

    SDSTOR-7548 : Fixing std::bad_cast (#1083)
    link: https://jirap.corp.ebay.com/browse/SDSTOR-7548
    
    In the test_log_store, there is an inconsistency in SISL_OPTION['device_list'] where it is set as a default value of string and called as the vector of strings. This results in throwing core dump as bad_cast. Using this change fixes the problem as the following example (bin/test_log_store --iterations=10  --device_list traddr=0000:3b:00.0):
    
    (gdb) p m_dev_names
    (gdb)$1 = std::vector of length 1, capacity 1 = {"traddr=0000:3b:00.0"}

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "3.4.4"
+    version = "3.4.5"
     homepage = "https://github.corp.ebay.com/SDS/homestore"
     description = "HomeStore"
     topics = ("ebay", "nublox")
