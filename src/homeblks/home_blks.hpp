e1e8819b1eea0fd50496a516ecfac6431ce448ec
Yaming Kuang <yaming.kuang@gmail.com>
2022-12-15 15:52:58 -0700

    incorperate comments

--- src/homeblks/home_blks.hpp
+++ src/homeblks/home_blks.hpp
@@ -264,7 +264,7 @@ public:
 
     bool is_recovery_mode() { return !m_rdy; }
     bool is_shutdown() const { return (m_shutdown_start_time.load(std::memory_order_acquire) != 0); }
-    virtual bool is_destroying(const VolumePtr vol) const;
+    virtual bool is_destroying(const VolumePtr vol) const override;
 
     void init_done();
     void inc_sub_system_init_cnt();
