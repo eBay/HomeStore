dd8c07891500b7f5f2249470199b5cd94a00e5eb
Kadayam, Harihara(hkadayam) <hkadayam@ebay.com>
2022-12-16 11:39:14 -0800

    Remove author names from the header

--- src/engine/blkalloc/varsize_blk_allocator.cpp
+++ src/engine/blkalloc/varsize_blk_allocator.cpp
@@ -1,8 +1,6 @@
 /*********************************************************************************
  * Modifications Copyright 2017-2019 eBay Inc.
  *
- * Author/Developer(s):
- * Author/Developer(s): Harihara Kadayam, Bryan Zimmerman, Rishabh Mittal
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
