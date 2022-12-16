4d1850facd4e05620a3abf1958419e0bf009f565
Kadayam, Harihara(hkadayam) <hkadayam@ebay.com>
2022-12-16 12:44:33 -0800

    Updated copyright header

--- src/lib/common/homestore_status_mgr.hpp
+++ src/lib/common/homestore_status_mgr.hpp
@@ -1,7 +1,6 @@
 /*********************************************************************************
  * Modifications Copyright 2017-2019 eBay Inc.
  *
- * Author/Developer(s): Ravi Nagarjuna Akella
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
