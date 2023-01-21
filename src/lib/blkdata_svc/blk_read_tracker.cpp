ca54c44c0aa9641e930a6fe971858b7d3223bc35
Yaming Kuang <yaming.kuang@gmail.com>
2023-01-20 17:18:03 -0700

    SDSTOR-8781 Read Blk Tracker

--- src/lib/blkdata_svc/blk_read_tracker.cpp
+++ src/lib/blkdata_svc/blk_read_tracker.cpp
@@ -1,7 +1,6 @@
 /*********************************************************************************
  * Modifications Copyright 2017-2019 eBay Inc.
  *
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,69 +17,91 @@
 namespace homestore {
 static BlkId extract_key(const BlkTrackRecord& rec) { return rec.m_key; }
 
-static bool blkid_overlaps(const BlkId& a, const BlkId& b) {
-    if (a.get_chunk_num() != b.get_blk_num()) { return false; }
-    if (a.get_blk_num() == b.get_blk_num()) { return true; }
-    if (a.get_blk_num < b.get_blk_num()) { return ((a.get_blk_num() + a.get_nblks()) >= b.get_blk_num()); }
-    return ((b.get_blk_num() + b.get_nblks()) >= a.get_blk_num());
-}
-
-static bool merge_records(BlkTrackRecord& rec, const BlkTrackRecord& other) {
-    assert(rec.m_key == other.m_key);
-    assert(other.m_sub_record.size() == 1); // We don't support multiple elements to merge with each other
-
-    const auto& new_rec = other.m_sub_record[0];
-    rec.m_total_refcount += other.m_total_refcount;
-
-    bool inserted{false};
-    auto it = rec.m_sub_record.begin();
-    while (it != rec.m_sub_record.end()) {
-        if (blkid_overlaps(it->sub_range, new_rec.sub_range)) {
-            it->ref_count += new_rec.ref_count;
-            if (it->ref_count == 0) { // No one references, removed from list
-                it = rec.m_sub_record.erase(it);
-            } else {
-                it->waiter = new_rec.waiter;
-                inserted = true;
-            }
-        }
-        ++it;
-    }
-
-    if (!inserted) { rec.m_sub_record.push_back(new_rec); }
-    return (rec.m_total_refcount == 0);
-}
-
-BlkReadTracker::BlkReadTracker() : m_pending_reads_map(expected_num_records, extract_key, nullptr, merge_records) {}
+BlkReadTracker::BlkReadTracker() : m_pending_reads_map(s_expected_num_records, extract_key, nullptr /* access_cb */) {}
 
 void BlkReadTracker::merge(const BlkId& blkid, int64_t new_ref_count,
                            const std::shared_ptr< blk_track_waiter >& waiters) {
-    auto cur_blk_num = blkid.get_blk_num();
-    auto max_this_entry = entries_per_record - (cur_blk_num - sisl::round_down(cur_blk_num, entries_per_record));
+    if (new_ref_count == 0) {
+        assert(waiters);
+    } else {
+        assert(!waiters);
+    }
 
-    while (cur_blk_num <= blkid.get_last_blk_num()) {
-        const auto count = std::min(max_this_entry, (blkid.get_last_blk_num() - cur_blk_num + 1));
+    //
+    // Don't move alignment handling outside of this function, because the nblks between (first and last blk num after
+    // alignment) could be larger than 255 which exceeds a BlkId can hold;
+    //
+    auto cur_blk_num_aligned = s_cast< blk_num_t >(sisl::round_down(blkid.get_blk_num(), entries_per_record()));
+    auto last_blk_num_aligned_up = s_cast< blk_num_t >(sisl::round_up(blkid.get_last_blk_num(), entries_per_record()) -
+                                                       1); //  -1 so that it does not cover next base id;
+    if (blkid.get_last_blk_num() % entries_per_record() == 0) {
+        // if last blk num happens to be aligned, it actually belongs to next base id, so add 1 back;
+        last_blk_num_aligned_up += 1;
+    }
 
-        BlkId base_blkid{sisl::round_down(cur_blk_num, entries_per_record), entries_per_record, blkid.get_chunk_num()};
-        BlkId sub_blkid{cur_blk_num, count, blkid.get_chunk_num()};
+    // everything is aligned after this point, so we don't need to handle sub_range in a base blkid;
+    while (cur_blk_num_aligned <= last_blk_num_aligned_up) {
+        BlkId base_blkid{cur_blk_num_aligned, entries_per_record(), blkid.get_chunk_num()};
 
         BlkTrackRecord rec;
-        rec.m_key = base_blkid;
-        rec.m_total_refcount = new_ref_count;
-        rec.m_sub_record.emplace_back(sub_blkid, new_ref_count, waiters);
+        const auto rec_found = m_pending_reads_map.get(base_blkid, rec);
 
-        m_pending_reads_map.merge(base_blkid, rec);
+        if (new_ref_count != 0) {
+            // this is insert/remove operations
+            if (rec_found) {
+                // if some read is already happening on this record, just update the ref_cnt;
+                rec.m_ref_cnt += new_ref_count;
+            } else {
+                // if no record found, no read is happening on this record;
+                rec.m_key = base_blkid;
+                rec.m_ref_cnt = new_ref_count;
+            }
 
-        cur_blk_num += count;
-        max_this_entry = entries_per_record;
+            // in either case, ref_cnt can not drop below zero;
+            assert(rec.m_ref_cnt >= 0);
+
+            if (rec.m_ref_cnt > 0) {
+                m_pending_reads_map.upsert(base_blkid, rec);
+            } else {
+                // ref_cnt drops to zer, clear all the references held by this record;
+                assert(rec_found);
+                rec.m_waiters.clear();
+                BlkTrackRecord dummy_rec;
+                m_pending_reads_map.erase(base_blkid, dummy_rec);
+            }
+        } else {
+            // this is wait_on operation
+            if (rec_found) {
+                // apply waiter to this record;
+                rec.m_waiters.push_back(waiters);
+                // overwirte existing record;
+                m_pending_reads_map.upsert(base_blkid, rec);
+            }
+
+            // not found, nothing needs to be done; fall through and visit remaining records;
+        }
+
+        cur_blk_num_aligned += entries_per_record();
     }
+
+    // if no record is found for a wait-on operation, it means no one is holding reference for this waiter and cb will
+    // be called automatically when this function exits (waiter's destrctor will be called);
 }
 
 void BlkReadTracker::insert(const BlkId& blkid) { merge(blkid, 1, nullptr); }
-
 void BlkReadTracker::remove(const BlkId& blkid) { merge(blkid, -1, nullptr); }
 
-void BlkReadTracker::ensure_no_record(const BlkId& blkid, after_remove_cb_t&& after_remove_cb) {
+void BlkReadTracker::wait_on(const BlkId& blkid, after_remove_cb_t&& after_remove_cb) {
     merge(blkid, 0, std::make_shared< blk_track_waiter >(std::move(after_remove_cb)));
 }
+
+uint16_t BlkReadTracker::entries_per_record() const {
+    // TODO: read from config;
+    return m_entries_per_record;
+}
+
+#ifdef _PRERELEASE
+void BlkReadTracker::set_entries_per_record(uint16_t num_entries) { m_entries_per_record = num_entries; }
+#endif
+
 } // namespace homestore
