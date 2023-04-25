88c629d8f730deb4a8a79ab7d44353727ca4861c
shosseinimotlagh <shosseinimotlagh@ebay.com>
2023-04-25 14:18:29 -0700

    test

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "4.0.11"
+    version = "4.0.12"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
