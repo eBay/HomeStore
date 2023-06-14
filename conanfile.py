2f4bacc3cd7071a34340c72b76ef3daaeaa3c4e3
Brian Szmyd <bszmyd@ebay.com>
2023-06-14 10:18:04 -0700

    IOmgr is now .2

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class HomestoreConan(ConanFile):
         self.build_requires("gtest/1.12.1")
 
     def requirements(self):
-        self.requires("iomgr/8.7.1")
+        self.requires("iomgr/8.7.2")
         self.requires("sisl/8.5.4") # sisl: stable/v8.x
 
         # FOSS, rarely updated
