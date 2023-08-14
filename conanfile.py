b7675d1fbc0031e936b7d14451dc9c1a3b90926f
shosseinimotlagh <shosseinimotlagh@ebay.com>
2023-08-14 10:26:56 -0700

    Enable sanitize

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,7 @@ class HomestoreConan(ConanFile):
                 'shared': False,
                 'fPIC': True,
                 'coverage': False,
-                'sanitize': False,
+                'sanitize': True,
                 'testing': 'epoll_mode',
                 'skip_testing': False,
                 'sisl:prerelease': True,
