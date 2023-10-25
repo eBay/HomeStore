baf81c930083335b014c85aa64a217969cae9d3b
Yaming Kuang <1477567+yamingk@users.noreply.github.com>
2023-10-25 04:02:04 -0700

    get defrag nblks api (#209)

--- src/include/homestore/homestore.hpp
+++ src/include/homestore/homestore.hpp
@@ -127,6 +127,8 @@ private:
     hs_before_services_starting_cb_t m_before_services_starting_cb{nullptr};
     bool m_init_done{false};
 
+    bool m_init_done{false};
+
 public:
     HomeStore() = default;
     virtual ~HomeStore() = default;
