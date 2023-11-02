9dc9130f060c64cd92fafeac1e260721f5111f35
Yaming Kuang <1477567+yamingk@users.noreply.github.com>
2023-11-01 18:27:34 -0700

    Issue 187#: Long duration test for data service (#220)
    * issue: 187# Long duration data service load test case.
    
    1. add long duration test for data service with random read, write and free.
    2. fixed one issue in MultiBlkId iterator
    3. add/update api comments for data service

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "4.5.9"
+    version = "4.5.10"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
