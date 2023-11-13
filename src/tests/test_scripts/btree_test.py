02b16cca7ed27e519932c4a5c21119d880308bc7
Mehdi Hosseini <116847813+shosseinimotlagh@users.noreply.github.com>
2023-11-13 12:56:31 -0800

    Long running for index service. (#219)
    * Main changes
    
    add concurrent UT for index tree
    change btree_test.py (add device list and run time)
    introduce run_time

--- src/tests/test_scripts/btree_test.py
+++ src/tests/test_scripts/btree_test.py
@@ -12,16 +12,18 @@ import requests
 
 opts, args = getopt.getopt(sys.argv[1:], 'tdlme:',
                            ['test_suits=', 'dirpath=', 'op_list=', 'log_mods=', 'threads=', 'fibers=', 'preload_size=',
-                            'op_list=', 'num_entries=', 'num_iters='])
+                            'op_list=', 'num_entries=', 'num_iters=', 'dev_list=', 'run_time='])
 test_suits = ""
 dirpath = "./"
 op_list = ""
 log_mods = ""
-threads = " --n_threads=10"
-fibers = " --n_fibers=10"
-preload_size = "  --preload_size=2000"
-num_entries = " --num_entries=10000"
-num_iters = " --num_iters=1000000"
+threads = " --num_threads=10"
+fibers = " --num_fibers=10"
+preload_size = "  --preload_size=16384"
+num_entries = " --num_entries=65536"
+num_iters = " --num_iters=10000000"
+run_time = " --run_time=36000"
+dev_list = ""
 
 for opt, arg in opts:
     if opt in ('-t', '--test_suits'):
@@ -38,13 +40,13 @@ for opt, arg in opts:
         log_mods = arg
         print("log_mods (%s)" % arg)
     if opt in ('-f', '--fibers'):
-        fibers = " --n_fibers=" + arg
+        fibers = " --num_fibers=" + arg
         print("number of fibers per thread (%s)" % arg)
     if opt in ('-p', '--preload_size'):
         preload_size = " --preload_size=" + arg
         print("preload_size = (%s)" % arg)
     if opt in ('-t', '--threads'):
-        threads = " --n_threads=" + arg
+        threads = " --num_threads=" + arg
         print("number of threads (%s)" % arg)
     if opt in ('-n', '--num_entries'):
         num_entries = " --num_entries=" + arg
@@ -52,19 +54,30 @@ for opt, arg in opts:
     if opt in ('-i', '--num_iters'):
         num_iters = " --num_iters=" + arg
         print("number of iterations (%s)" % arg)
+    if opt in ('-r', '--run_time'):
+        run_time = " --run_time=" + arg
+        print("total run time (%s)" % arg)
+    if opt in ('-v', '--dev_list'):
+        dev_list = arg
+        print(("device list (%s)") % (arg))
 
 operations = ""
 if bool(op_list and op_list.strip()):
     operations = ''.join([f' --operation_list={op}' for op in op_list.split()])
 
-btree_options = num_entries + num_iters + preload_size + fibers + threads + operations
+addln_opts = ' '
+if bool(dev_list and dev_list.strip()):
+    addln_opts += ' --device_list '
+    addln_opts += dev_list
+
+btree_options = num_entries + num_iters + preload_size + fibers + threads + operations + run_time + addln_opts
 
 
 def normal():
     print("normal test started with (%s)" % btree_options)
     # " --operation_list=query:20 --operation_list=put:20 --operation_list=remove:20"
-    cmd_opts = " --gtest_filter=BtreeConcurrentTest/*.AllTree" + btree_options + " "+log_mods
-    subprocess.check_call(dirpath + "test_mem_btree " + cmd_opts, stderr=subprocess.STDOUT, shell=True)
+    cmd_opts = " --gtest_filter=BtreeConcurrentTest/*.ConcurrentAllOps" + btree_options + " "+log_mods
+    subprocess.check_call(dirpath + "test_index_btree " + cmd_opts, stderr=subprocess.STDOUT, shell=True)
     print("normal test completed")
 
 
