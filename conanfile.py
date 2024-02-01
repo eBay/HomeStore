68a01f67de999c05b210600b786e4a34b009f7e4
Jie Yao <jyao3@ebay.com>
2024-01-31 17:36:32 -0700

    update conan version

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,7 @@ from conans import CMake
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "5.0.8"
-
+    version = "5.0.9"
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
     topics = ("ebay", "nublox")
