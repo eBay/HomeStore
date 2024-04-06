3a06119623e39cd158e27e7b905015597ce1e380
Yaming Kuang <yaming.kuang@gmail.com>
2024-04-05 18:03:19 -0700

    bump conan ver

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.1.0"
+    version = "6.3.1"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
