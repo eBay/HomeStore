3ab1895708fb7649539c798fa45b46e739ec7660
yuwmao <yuwmao@ebay.com>
2024-06-24 17:10:38 +0800

    Remove AppendBlkAllocMetrics

--- src/lib/blkalloc/append_blk_allocator.cpp
+++ src/lib/blkalloc/append_blk_allocator.cpp
@@ -22,7 +22,7 @@
 namespace homestore {
 
 AppendBlkAllocator::AppendBlkAllocator(const BlkAllocConfig& cfg, bool need_format, allocator_id_t id) :
-        BlkAllocator{cfg, id}, m_metrics{get_name().c_str()} {
+        BlkAllocator{cfg, id} {
     // TODO: try to make all append_blk_allocator instances use same client type to reduce metablk's cache footprint;
     meta_service().register_handler(
         get_name(),
@@ -68,12 +68,12 @@ BlkAllocStatus AppendBlkAllocator::alloc_contiguous(BlkId& bid) { return alloc(1
 //
 BlkAllocStatus AppendBlkAllocator::alloc(blk_count_t nblks, const blk_alloc_hints& hint, BlkId& out_bid) {
     if (available_blks() < nblks) {
-        COUNTER_INCREMENT(m_metrics, num_alloc_failure, 1);
+        //COUNTER_INCREMENT(m_metrics, num_alloc_failure, 1);
         LOGERROR("No space left to serve request nblks: {}, available_blks: {}", nblks, available_blks());
         return BlkAllocStatus::SPACE_FULL;
     } else if (nblks > max_blks_per_blkid()) {
         // consumer(vdev) already handles this case.
-        COUNTER_INCREMENT(m_metrics, num_alloc_failure, 1);
+        //COUNTER_INCREMENT(m_metrics, num_alloc_failure, 1);
         LOGERROR("Can't serve request nblks: {} larger than max_blks_in_op: {}", nblks, max_blks_per_blkid());
         return BlkAllocStatus::FAILED;
     }
@@ -81,7 +81,7 @@ BlkAllocStatus AppendBlkAllocator::alloc(blk_count_t nblks, const blk_alloc_hint
     // Push 1 blk to the vector which has all the requested nblks;
     out_bid = BlkId{m_last_append_offset.fetch_add(nblks), nblks, m_chunk_id};
 
-    COUNTER_INCREMENT(m_metrics, num_alloc, 1);
+    //COUNTER_INCREMENT(m_metrics, num_alloc, 1);
 
     return BlkAllocStatus::SUCCESS;
 }
