5cdae61aeddf24db1360d3ca9058d86ed2532574
Jie Yao <jyao3@ebay.com>
2024-06-30 04:31:19 -0700

    fix log flush bug

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.4.24"
+    version = "6.4.25"
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
     topics = ("ebay", "nublox")
