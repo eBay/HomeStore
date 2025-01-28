07d5a2f1bad3f112e36c51c00d92fe596530dd2e
Brian Szmyd <bszmyd@ebay.com>
2025-01-28 12:49:04 -0700

    Improve safe mode.
    Safe mode was previously not exitable. It would block indefinitely after
    receiving the signal and finishing init preventing the AM from finishing
    initialization. This change makes that possible.

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "3.8.0"
+    version = "3.8.1"
 
     homepage = "https://github.corp.ebay.com/SDS/homestore"
     description = "HomeStore"
