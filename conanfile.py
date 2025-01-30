e829ee2ece02cb9891bfc50d30990f6f2a395298
Ravi Nagarjun Akella <raakella1@$HOSTNAME>
2025-01-30 15:19:17 -0700

    update sisl to enable obj life counter in release builds. Revert the unstable OOM fix

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "3.7.6"
+    version = "3.7.7"
 
     homepage = "https://github.corp.ebay.com/SDS/homestore"
     description = "HomeStore"
@@ -56,7 +56,7 @@ class HomestoreConan(ConanFile):
 
     def requirements(self):
         self.requires("iomgr/8.8.1")
-        self.requires("sisl/8.9.1")
+        self.requires("sisl/8.9.2")
 
         # FOSS, rarely updated
         self.requires("boost/1.79.0")
