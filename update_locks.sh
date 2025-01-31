930ae5e4093955cc4305229a1c99341b66c1e59c
Brian Szmyd <bszmyd@ebay.com>
2025-01-31 10:34:45 -0700

    Fix locks

--- update_locks.sh
+++ update_locks.sh
@@ -5,12 +5,11 @@ BASE_LOCK=${LOCK_DIR}/base.lock
 DEBUG_LOCK=${LOCK_DIR}/debug_deps.lock
 RELEASE_LOCK=${LOCK_DIR}/release_deps.lock
 SANITIZE_LOCK=${LOCK_DIR}/sanitize_deps.lock
-COVERAGE_LOCK=${LOCK_DIR}/coverage_deps.lock
 
-#if [ ! -r ${BASE_LOCK} ]; then
-#    echo "No exisiting base lock! Incorrect working directory?"
-#    exit 1
-#fi
+if [ ! -r ${BASE_LOCK} ]; then
+    echo "No exisiting base lock! Incorrect working directory?"
+    exit 1
+fi
 
 echo -n "Updating Base Lock..."
 conan lock create --lockfile-out=${BASE_LOCK} --base ./conanfile.py  1>/dev/null 2>&1
@@ -22,10 +21,6 @@ echo "done"
 
 echo -n "Updating Sanitize Lock..."
 conan lock create --lockfile-out ${SANITIZE_LOCK} --lockfile ${BASE_LOCK} -o homestore:sanitize=True -o sisl:prerelease=True -s:h build_type=Debug ./conanfile.py > /dev/null
-echo "done"
-
-echo -n "Updating Coverage Lock..."
-conan lock create --lockfile-out ${COVERAGE_LOCK} --lockfile ${BASE_LOCK} -o homestore:coverage=True -o sisl:prerelease=True -s:h build_type=Debug ./conanfile.py > /dev/null
 echo "done"
 
 echo -n "Updating Release Lock..."
