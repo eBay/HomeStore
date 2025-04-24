b0185ed82a779f29d41add169bb262c364cf47ea
Yaming Kuang <yaming.kuang@gmail.com>
2025-04-24 11:10:27 -0700

    call destroy repldev listener for raft repl dev

--- src/lib/replication/service/raft_repl_service.cpp
+++ src/lib/replication/service/raft_repl_service.cpp
@@ -412,6 +412,7 @@ folly::SemiFuture< ReplServiceError > RaftReplService::remove_repl_dev(group_id_
     }
 
     auto ret = std::dynamic_pointer_cast< RaftReplDev >(rdev_result.value())->destroy_group();
+
     decr_pending_request_num();
     return ret;
 }
