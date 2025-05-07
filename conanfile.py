ad41e3557a4725cbd2fb36b11e4a02dd77a22282
Yaming Kuang <yaming.kuang@gmail.com>
2025-05-07 10:46:24 -0700

    Fix log periodic cancelt_imer issue and solo repl dev init/destroy race issue

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.13.8"
+    version = "6.13.9"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
