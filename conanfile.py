575d8c153f36fe235c93fa95b4e531713efd5849
Jie Yao <jyao3@ebay.com>
2025-06-04 08:06:59 +0800

    add get_used_blk for vchunk (#733)

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.15.4"
+    version = "6.16.0"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
