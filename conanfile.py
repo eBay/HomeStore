d2adbc9c5aea7ba01a0ddb41817c0ef52da7d765
yawzhang <yawzhang@ebay.com>
2025-06-06 16:26:10 +0800

    determine proposer based on server_id

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.16.2"
+    version = "6.16.3"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
