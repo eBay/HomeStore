fc7f684fedc58efdd49f00c9d2a7be113ffa6e09
Yaming Kuang <yaming.kuang@gmail.com>
2025-07-16 14:23:14 -0700

    update con

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.19.0"
+    version = "6.20.0"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
