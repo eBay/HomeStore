8dd83e867006a068a0f7b3516eb75aa94df0b7b8
Jie Yao <jyao3@ebay.com>
2025-08-11 15:29:39 +0800

    support disable push_data by rreq (#790)

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "6.20.14"
+    version = "6.20.15"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
