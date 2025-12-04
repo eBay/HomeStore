4d4f6aa514034c32c87204a005ea39894dd5b76c
yawzhang <yawzhang@ebay.com>
2025-12-04 17:20:39 +0800

    Fix : handle the case when rreq is nullptr at commit_ext

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "7.0.8"
+    version = "7.0.9"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
