694a591718728eeda72017c07eb54691a6a08cc5
yawzhang <yawzhang@ebay.com>
2026-02-05 11:34:04 +0800

    Fix UT: address UAF in TestRaftLogStore lifecycle_test under ASan

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomestoreConan(ConanFile):
     name = "homestore"
-    version = "7.4.2"
+    version = "7.4.3"
 
     homepage = "https://github.com/eBay/Homestore"
     description = "HomeStore Storage Engine"
