ff106e11eca8c61793268f5c8ff4a4d1378f9947
yuwmao <yuwmao@ebaychina.com>
2026-03-17 14:35:20 +0800

    handle comments

--- src/lib/index/wb_cache.cpp
+++ src/lib/index/wb_cache.cpp
@@ -831,7 +831,9 @@ folly::Future< bool > IndexWBCache::async_cp_flush(IndexCPContext* cp_ctx) {
 #ifdef _PRERELEASE
     LOGTRACEMOD(wbcache, "Starting Index CP Flush with cp \ndag={}", cp_ctx->to_string_with_dags());
 #else
-    LOGINFOMOD(wbcache, "Starting Index CP Flush with cp {}", cp_ctx->id());
+    LOGINFOMOD(wbcache, "Starting Index CP Flush with cp {}, dirty_buf_count={}, nodes_added={}, nodes_removed={}",
+               cp_ctx->id(), cp_ctx->m_dirty_buf_count.get(), cp_ctx->m_num_nodes_added.load(),
+               cp_ctx->m_num_nodes_removed.load());
 #endif
     // #ifdef _PRERELEASE
     //     static int id = 0;
